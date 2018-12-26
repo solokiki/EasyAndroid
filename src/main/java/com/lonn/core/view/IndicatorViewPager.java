b916473eaecfaeb3ea6d2f40ed8fa3070ffa9399
chenlong <chenlong@sh-tramy.com>
2018-12-26 10:36:31 +0800

    修复EventBus打包问题

--- src/main/java/com/lonn/core/view/IndicatorViewPager.java
+++ src/main/java/com/lonn/core/view/IndicatorViewPager.java
@@ -7,23 +7,33 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.lonn.core.R;
+import com.lonn.core.utils.AppUtil;
 
 public class IndicatorViewPager extends RelativeLayout {
 
     public static final int DEFAULT_ITEM = 1;
+
+    public static final int INDICATOR_STYLE_NONE = 1;
+    public static final int INDICATOR_STYLE_DOT = 2;
+    public static final int INDICATOR_STYLE_NUMBER = 3;
 
     private Context mContext;
 
     private AutoScrollViewPager viewPager;
     private PagerAdapter adapter;
 
-    private LinearLayout dotLayout;
+    private LinearLayout indicatorLayout;
     private ImageView[] dots;
+    private TextView indicatorNumber;
+
+    private int indicatorStyle = INDICATOR_STYLE_DOT;
 
     private int resNormal = R.drawable.lonn_dot_gray_normal;
     private int resCurrent = R.drawable.lonn_dot_gray_current;
@@ -50,8 +60,9 @@ public class IndicatorViewPager extends RelativeLayout {
         this.mContext = context;
 
         viewPager = new AutoScrollViewPager(context);
-        dotLayout = new LinearLayout(context);
-        dotLayout.setGravity(Gravity.CENTER);
+        indicatorLayout = new LinearLayout(context);
+        indicatorLayout.setGravity(Gravity.CENTER);
+        indicatorLayout.setPadding(10,0,10,0);
 
         LayoutParams viewPagerParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         LayoutParams dotLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, 80);
@@ -59,7 +70,7 @@ public class IndicatorViewPager extends RelativeLayout {
         dotLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
 
         addView(viewPager, viewPagerParams);
-        addView(dotLayout, dotLayoutParams);
+        addView(indicatorLayout, dotLayoutParams);
 
         // 事件监听
         setOnPageChangeListener(onPageChangeListener);
@@ -72,45 +83,73 @@ public class IndicatorViewPager extends RelativeLayout {
             viewPager.setAdapter(adapter);
             viewPager.setCurrentItem(currentIndex);
         }
-        initDots();
+        initIndicator();
     }
 
     /**
-     * @Description: 初始化显示小圆点。当小圆点数量小于2时，不显示小圆点。
+     * 设置指示器样式
+     * @param style
      */
-    private void initDots() {
-        dotLayout.removeAllViews();
+    public void setIndicatorStyle(int style){
+        this.indicatorStyle = style;
+    }
+
+    /**
+     * 设置指示器位置
+     * @param gravity
+     */
+    public void setIndicatorGravity(int gravity){
+        indicatorLayout.setGravity(gravity);
+    }
+
+    /**
+     * @Description: 初始化轮播指示器
+     */
+    private void initIndicator() {
+        indicatorLayout.removeAllViews();
 
         if (adapter == null) {
             return;
         }
 
-        LinearLayout.LayoutParams dotParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        dotParams.leftMargin = 10;
+        if(indicatorStyle == INDICATOR_STYLE_NONE){
+            indicatorLayout.setVisibility(View.GONE);
+        }else if(indicatorStyle == INDICATOR_STYLE_DOT){
+            LinearLayout.LayoutParams indicatorParams = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            indicatorParams.leftMargin = 10;
 
-        int count = adapter.getCount() - 2;
-        if (count < 2) {
-            return;
+            // 当数量小于2时，不显示小圆点
+            int count = getCount();
+            if (count < 2) {
+                return;
+            }
+
+            dots = new ImageView[count];
+            for (int i = 0; i < dots.length; i++) {
+                ImageView iv = new ImageView(mContext);
+                iv.setImageResource(resNormal);
+                iv.setLayoutParams(indicatorParams);
+                dots[i] = iv;
+                indicatorLayout.addView(iv);
+            }
+            setDot();
+        }else if(indicatorStyle == INDICATOR_STYLE_NUMBER){
+            indicatorNumber = new TextView(mContext);
+            indicatorNumber.setTextColor(AppUtil.getColor(mContext,R.color.white));
+            indicatorNumber.setBackgroundColor(AppUtil.getColor(mContext,R.color.black));
+            indicatorLayout.addView(indicatorNumber);
+            setNumber();
         }
 
-        dots = new ImageView[count];
-        for (int i = 0; i < dots.length; i++) {
-            ImageView iv = new ImageView(mContext);
-            iv.setImageResource(resNormal);
-            iv.setLayoutParams(dotParams);
-            dots[i] = iv;
-            dotLayout.addView(iv);
-        }
 
-        setDot();
     }
 
     /**
      * @param color 十六进制颜色
      * @Description: 设置小圆点区域背景颜色。当小圆点数量小于2时，不显示小圆点，所以此时设置无效。
      */
-    public void setDotLayoutBackgroundColor(String color) {
-        if (dotLayout == null || adapter == null || TextUtils.isEmpty(color)) {
+    public void setIndicatorBackgroundColor(String color) {
+        if (indicatorLayout == null || adapter == null || TextUtils.isEmpty(color)) {
             return;
         }
 
@@ -118,14 +157,14 @@ public class IndicatorViewPager extends RelativeLayout {
             return;
         }
 
-        dotLayout.setBackgroundColor(Color.parseColor(color));
+        indicatorLayout.setBackgroundColor(Color.parseColor(color));
     }
 
     /**
-     * 设置小圆点区域背景
+     * 设置指示器背景
      */
-    public void setDotLayoutBackgroundResource(int resId) {
-        if (dotLayout == null || adapter == null) {
+    public void setIndicatorBackgroundResource(int resId) {
+        if (indicatorLayout == null || adapter == null) {
             return;
         }
 
@@ -133,14 +172,14 @@ public class IndicatorViewPager extends RelativeLayout {
             return;
         }
 
-        dotLayout.setBackgroundResource(resId);
+        indicatorLayout.setBackgroundResource(resId);
     }
 
     /**
-     * 设置小圆点区域尺寸
+     * 设置指示器背景大小
      */
-    public void setDotLayoutSize(int width, int height) {
-        if (dotLayout == null || adapter == null) {
+    public void setIndicatorSize(int width, int height) {
+        if (indicatorLayout == null || adapter == null) {
             return;
         }
 
@@ -148,8 +187,8 @@ public class IndicatorViewPager extends RelativeLayout {
             return;
         }
 
-        dotLayout.getLayoutParams().width = width;
-        dotLayout.getLayoutParams().height = height;
+        indicatorLayout.getLayoutParams().width = width;
+        indicatorLayout.getLayoutParams().height = height;
 
     }
 
@@ -180,26 +219,67 @@ public class IndicatorViewPager extends RelativeLayout {
         }
     }
 
+    private void setIndicator(){
+        switch (indicatorStyle) {
+            case INDICATOR_STYLE_DOT:
+                setDot();
+                break;
+            case INDICATOR_STYLE_NUMBER:
+                setNumber();
+                break;
+        }
+    }
+
     private void setDot() {
         if (dots == null || dots.length == 0) {
             return;
         }
         resetDots();
 
-        int index = 0;
-        if (currentIndex == 0) {
-            index = dots.length - 1;
-        } else if (currentIndex == adapter.getCount() - 1) {
-            index = 0;
-        }else{
-            index = currentIndex - 1;
-        }
+//        int index = 0;
+//        if (currentIndex == 0) {
+//            index = dots.length - 1;
+//        } else if (currentIndex == adapter.getCount() - 1) {
+//            index = 0;
+//        }else{
+//            index = currentIndex - 1;
+//        }
+
+        int index = getRealIndex();
 
         if(index >= 0 && index < dots.length){
             dots[index].setImageResource(resCurrent);
         }
+    }
 
+    private void setNumber(){
+        if(indicatorNumber != null){
+            indicatorNumber.setText((getRealIndex()+1) + "/" + getCount());
+        }
+    }
 
+    /**
+     * 获取当前滑动的页数
+     * @return
+     */
+    private int getRealIndex(){
+        int index = 0;
+        if (currentIndex == 0) {
+            index = getCount() - 1;
+        } else if (currentIndex == getCount() + 1) {
+            index = 0;
+        }else{
+            index = currentIndex - 1;
+        }
+        return index;
+    }
+
+    /**
+     * 获取总数
+     * @return
+     */
+    private int getCount(){
+        return adapter.getCount() - 2;
     }
 
     /**
@@ -225,7 +305,7 @@ public class IndicatorViewPager extends RelativeLayout {
         @Override
         public void onPageSelected(int arg0) {
             currentIndex = arg0;
-            setDot();
+            setIndicator();
 
             if (onPagerSelectListener != null) {
                 onPagerSelectListener.onPageSelect(arg0);
